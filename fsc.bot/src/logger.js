ab880f2904760cdf38eda383d733ab97e3a00386
Brian Morrison II <brian@brianmorrison.me>
2021-03-30 08:42:58 -0500

    Walter - Added cloudwatch logger, enabled xp decrement

---
+++ fsc.bot/src/logger.js
@@ -0,0 +1,27 @@
+const winston = require('winston');
+const WinstonCloudWatch = require('winston-cloudwatch');
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  region: 'us-east-1',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new WinstonCloudWatch({
+      cloudWatchLogs: new AWS.CloudWatchLogs(),
+      logGroupName: 'fullstack-chat', 
+      logStreamName: 'walter'
+    }),
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  ],
+});
+
+module.exports = logger
