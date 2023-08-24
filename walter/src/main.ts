ad6563aa10c1e9c1b1cf4ea45949144637364941
Brian Morrison II <brian@planetscale.com>
2023-08-24 10:25:21 -0400

    cleanup

--- walter/src/main.ts
+++ walter/src/main.ts
@@ -8,7 +8,6 @@ import { CommandManager } from './models';
 // Commands
 import { dadJoke } from './commands/dadjoke'
 
-// require('custom-env').env(true)
 const client = new Client({
   intents: [
     GatewayIntentBits.DirectMessages,
@@ -18,7 +17,6 @@ const client = new Client({
     GatewayIntentBits.GuildModeration
   ]
 });
-// const scheduler = require('@brianmmdev/script-scheduler')
 
 // const xpService = require('./services/xpService')
 // const portfolioService = require('./services/portfolioService')
@@ -32,9 +30,7 @@ client.on("ready", async () => {
   try {
     // if (process.env.IS_XP_ENABLED) {
     //   await xpService.init()
-    //   // await portfolioService.init()
     // }
-    // commands = await parseCommands('./src/commands')
     commandManager.registerCommand(dadJoke)
 
     log.info('Registered commands are:\n')
@@ -54,8 +50,7 @@ client.on("error", (e) => {
 //   await security.sendModBroadcast(member.guild, `**${member.user.username}** just joined **${member.guild.name}**!`)
 // });
 
-client.on("message", async (message) => {
-  console.log(message)
+client.on(Events.MessageCreate, async (message) => {
   if(message.author.bot) {
     return;
   }
@@ -64,55 +59,9 @@ client.on("message", async (message) => {
   //   xpService.logXp(message, message.author.id, message.author.username)
   // }
 
-  console.log(message.content)
-
   if(message.content.startsWith(prefix)) {
-    // const cmd = message.content.split(' ')[1];
-
-    // // Handle global `help` cmd
-    // if(cmd && cmd.toLowerCase() === 'help') {
-    //   await message.author.send(commandManager.getHelpText())
-    //   await message.delete()
-    //   return;
-    // }
-
     await commandManager.handleMessage(message)
-
-  
-
-    // // Return on unknown commands
-    // if (commands[cmd] === undefined) {
-    //   log.info('Unable to find command', cmd)
-    //   return
-    // }
-
-    // // Handle command-based help
-    // const subCommand = message.content.split(' ')[2]
-    // if(subCommand && subCommand.toLowerCase() === 'help') {
-    //   if(commands[cmd].helpText) {
-    //     let helpResponse = `**!w ${commands[cmd].command} help:**`
-    //     helpResponse += '\n```yaml'
-    //     helpResponse += `${commands[cmd].helpText}\n`
-    //     helpResponse += '```\n'
-    //     await message.author.send(helpResponse)
-    //     await message.delete()
-    //     return
-    //   }
-    // }
-
-    // // Handle command
-    // await commands[cmd].fn(message);
-
-    // if(commands[cmd].shouldCleanup) {
-    //   await message.delete()
-    // }
   }
 });
 
-// client.on("guildMemberAdd")
-
 client.login(process.env.BOT_TOKEN);
-
-// Setup scheduled scripts
-// const baseDir = `${__dirname}/scripts/`
-// scheduler.run(baseDir)
