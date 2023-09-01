0871c43575c13b995dc7fed9f64d41fbcb31fe9c
Ryan Furrer <ryanfurrer@hey.com>
2023-09-01 17:09:35 -0400

    Fix mobile navmanu being cutoff
    Changed ul height from 100vh to 100dvh.
    
    Fix could not be tested just yet due to "on your network" not working on my phone. Will try again when I get home.

--- website/src/components/Navbar.tsx
+++ website/src/components/Navbar.tsx
@@ -29,7 +29,7 @@ function Navbar() {
 			<ul
 				className={`${
 					isMenuShown ? `absolute` : `hidden`
-				} top-0 left-0 w-full h-screen text-right p-4 bg-[#14161c] gap-10 md:flex md:top-[unset] md:left-[unset] md:w-auto md:h-auto md:gap-8 justify-end`}
+				} top-0 left-0 w-full h-[100dvh] text-right p-4 bg-[#14161c] gap-10 md:flex md:top-[unset] md:left-[unset] md:w-auto md:h-auto md:gap-8 justify-end`}
 			>
 				<div className="container flex flex-col h-full justify-end gap-10 text-2xl md:flex-row md:h-auto md:gap-8 md:text-lg">
 					<li>
